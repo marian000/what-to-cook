bd1561300d6d554cedf19813483e2c9674a10cda
Marian Puțirac <marian93nes@gmail.com>
2023-08-31 21:04:36 +0300

    Basic structure
    - put tailwind
    - make a router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <div className="container mx-auto my-10">
+        <h1 className="text-3xl font-bold underline">This is a new tool for food ideas!</h1>
+
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button className="text-white bg-blue-700 px-10 py-3 rounded-full">Start from Here</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
